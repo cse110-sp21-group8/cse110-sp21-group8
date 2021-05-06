3aa9991ff18c3622eb525bd0a5d04aa86ab5b4ce
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2021-05-06 10:25:54 -0700

    change yml file

---
+++ index.js
@@ -0,0 +1,8 @@
+function greeting(name) {
+    return 'Hello' + name;
+}
+
+function broken() {
+  const u = 0;
+  u++;
+}
--- src/test.js
+++
@@ -1,8 +0,0 @@
-function greeting(name) {
-  return 'Hello' + name;
-}
-
-function broken() {
-  const let = 0;
-  let++;
-}
