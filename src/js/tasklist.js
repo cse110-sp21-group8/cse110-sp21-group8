864d5c7bbf15c80ee6f70076ba8b1930faba14ec
Marlon Trujillo <marlon.trujillo2001@gmail.com>
2021-05-21 14:15:18 -0700

    added tasklist custom element

---
+++ src/js/tasklist.js
@@ -0,0 +1,41 @@
+class TaskList extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <style>
+      .entry {
+        display: grid;
+        grid-template-columns: 25% auto;
+        width: 100%;
+      }
+      #text-box {
+        width: 100%;
+      }
+      #form {
+        display: grid;
+        grid-template-columns: 10% 10% 10% auto;
+      }
+    </style>
+    <div class=“entry”>
+      <select name=“Checklist Category” id=“checklist-select”>
+        <option value=“”>--Please choose an option--</option>
+        <option value=“Task”> Task </option>
+        <option value=“Event”> Event </option>
+        <option value=“Note”> Note </option>
+      </select>
+      <div id=“text-box”>
+        <form id=“form”>
+          <button id=“status”></button>
+          <button id=“delete”></button>
+          <button id=“move”></button>
+          <input type=“text” id=“tasks” placeholder=“Click to add tasks!“><br>
+        </form>
+      </div>
+    </div>
+    `;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+customElements.define('task-list', TaskList);
