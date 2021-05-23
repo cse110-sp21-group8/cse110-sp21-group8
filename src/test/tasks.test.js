7f15aa0ae228ad61a70ad55761023290e1ee9a55
Jinhao Zhou <j5zhou@ucsd.edu>
2021-05-23 15:11:03 -0700

    test cases for user tasks list: add, update, and delete

--- src/test/tasks.test.js
+++ src/test/tasks.test.js
@@ -21,7 +21,7 @@ describe('Test: fetch/get tasks', () => {
 
     test('User get their own daily task', done =>{
         //send the request
-        sendData = {status:"daily", date:"Fri May 21 2021"};
+        sendData = {status:"daily", date:"Sat May 22 2021"};
         request(app)
         .post("/getDailyTask").send(sendData)
         .set('Accept', 'application/json')
@@ -58,6 +58,58 @@ describe('Test: fetch/get tasks', () => {
     */
 });
 
+
+describe('Test: add tasks', () => {
+  test('User login successfully with correct password and username', done =>{
+      //send the request
+      let sendData={username:"admin@gmail.com",password:"123456"};
+      request(app)
+      .post("/user_login").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.user_status).toEqual(200);
+        done();
+      });
+  });
+
+  test('User add their own daily task', done =>{
+      //send the request
+      sendData = {status:"daily",type:"task",date:new Date().toDateString(),content:"test content"};
+      request(app)
+      .post("/addTask").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.status).toEqual(200);
+        done();
+      });
+  });
+
+  test('User add their own future task', done =>{
+    //send the request
+    sendData = {status:"future",type:"task",date:new Date().toDateString(),content:"test content"};
+    request(app)
+    .post("/addTask").send(sendData)
+    .set('Accept', 'application/json')
+    .then(response => {
+      expect(response.body.status).toEqual(200);
+      done();
+    });
+  });
+  test('User add their own monthly task', done =>{
+    //send the request
+    sendData = {status:"monthly",type:"task",date:new Date().toDateString(),content:"test content"};
+    request(app)
+    .post("/addTask").send(sendData)
+    .set('Accept', 'application/json')
+    .then(response => {
+      expect(response.body.status).toEqual(200);
+      done();
+    });
+  });
+
+});
+
+
 describe('Test: edit tasks', () => {
     test('User login successfully with correct password and username', done =>{
         //send the request
@@ -73,9 +125,11 @@ describe('Test: edit tasks', () => {
 
     test('User edit their own daily task', done =>{
         //send the request
-        sendData = {status:"daily", date:"Fri May 21 2021"};
+        sendData = { old:{status:"daily",type:"task",date:new Date().toDateString(),content:"test content"},new:{
+          status:"daily",type:"task",date:new Date().toDateString(),content:"11111"
+        }};
         request(app)
-        .post("/getDailyTask").send(sendData)
+        .post("/updateTask").send(sendData)
         .set('Accept', 'application/json')
         .then(response => {
           expect(response.body.status).toEqual(200);
@@ -83,29 +137,82 @@ describe('Test: edit tasks', () => {
         });
     });
 
-    /*
-    test('User get their own future task', done =>{
-        //send the request
-        sendData = {status:"future"};
-        request(app)
-        .post("/getDailyTask").send(sendData)
-        .set('Accept', 'application/json')
-        .then(response => {
-          expect(response.body.status).toEqual(200);
-          done();
-        });
+    test('User edit their own future task', done =>{
+      //send the request
+      sendData = { old:{status:"future",type:"task",date:new Date().toDateString(),content:"test content"},new:{
+        status:"future",type:"task",date:new Date().toDateString(),content:"11111"
+      }};
+      request(app)
+      .post("/updateTask").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.status).toEqual(200);
+        done();
+      });
     });
 
-    test('User get their own monthly task', done =>{
-        //send the request
-        sendData = {status:"monthly"};
-        request(app)
-        .post("/getDailyTask").send(sendData)
-        .set('Accept', 'application/json')
-        .then(response => {
-          expect(response.body.status).toEqual(200);
-          done();
-        });
+    test('User edit their own monthly task', done =>{
+      //send the request
+      sendData = { old:{status:"monthly",type:"task",date:new Date().toDateString(),content:"test content"},new:{
+        status:"monthly",type:"task",date:new Date().toDateString(),content:"11111"
+      }};
+      request(app)
+      .post("/updateTask").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.status).toEqual(200);
+        done();
+      });
     });
-    */
+});
+
+
+describe('Test: delete tasks', () => {
+  test('User login successfully with correct password and username', done =>{
+      //send the request
+      let sendData={username:"admin@gmail.com",password:"123456"};
+      request(app)
+      .post("/user_login").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.user_status).toEqual(200);
+        done();
+      });
+  });
+
+  test('User delete their own daily task', done =>{
+      //send the request
+      sendData = {status:"daily",type:"task",date:new Date().toDateString(),content:"11111"};
+      request(app)
+      .post("/deleteTask").send(sendData)
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.body.status).toEqual(200);
+        done();
+      });
+  });
+
+  test('User delete their own future task', done =>{
+    //send the request
+    sendData = {status:"future",type:"task",date:new Date().toDateString(),content:"11111"};
+    request(app)
+    .post("/deleteTask").send(sendData)
+    .set('Accept', 'application/json')
+    .then(response => {
+      expect(response.body.status).toEqual(200);
+      done();
+    });
+  });
+  test('User delete their own monthly task', done =>{
+    //send the request
+    sendData = {status:"monthly",type:"task",date:new Date().toDateString(),content:"11111"};
+    request(app)
+    .post("/deleteTask").send(sendData)
+    .set('Accept', 'application/json')
+    .then(response => {
+      expect(response.body.status).toEqual(200);
+      done();
+    });
+  });
+
 });
