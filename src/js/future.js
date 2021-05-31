046952e430432c89eeed60372eb02eb2baf5f0de
tangcb26 <tangcb26@gmail.com>
2021-05-31 01:42:50 -0700

    puppeteer-monthly(not finished)

--- src/js/future.js
+++ src/js/future.js
@@ -169,7 +169,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                     let newTask = data["task"];
                                     task.task_id = data.task._id;
                                 }else{
-                                    alert("Task didn't added");
+                                    //alert("Task didn't added");
                                 }
                             })
                             .catch((error) => {
@@ -196,7 +196,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 if(data["status"]==200){
                                     let newTask = data["task"];
                                 }else{
-                                    alert("Task didn't added");
+                                    //alert("Task didn't added");
                                 }
                             })
                             .catch((error) => {
@@ -222,7 +222,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 if(data["status"]==200){
                                     let newTask = data["task"];
                                 }else{
-                                    alert("Task didn't added");
+                                   // alert("Task didn't added");
                                 }
                             })
                             .catch((error) => {
@@ -284,7 +284,7 @@ window.onload = function(event){
                                 if(data["status"]==200){
                                     let newTask = data["task"];
                                 }else{
-                                    alert("Task didn't added");
+                                    //alert("Task didn't added");
                                 }
                             })
                             .catch((error) => {
