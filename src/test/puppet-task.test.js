8aaaa4190e628ac464285e114f361f9fb993cb3d
Jinhao Zhou <j5zhou@ucsd.edu>
2021-06-03 13:09:03 -0700

    adding more tesk for the daily task

--- src/test/puppet-task.test.js
+++ src/test/puppet-task.test.js
@@ -3,9 +3,9 @@
 describe('Daily Tasks tests:', () => {
     beforeAll(async () => {
       jest.setTimeout(35000);
-      await page.goto('http://localhost:8080');
+      await page.goto('http://localhost:8080/login');
       const username = await page.$('#email');
-      await username.type('admin');
+      await username.type('test');
 
       const password = await page.$('#password');
       await password.type('123');
@@ -65,17 +65,52 @@ describe('Daily Tasks tests:', () => {
     });
 
     //User add custom tag
-    it('Test2: User add custom tag', async () => {
-      let random_num = Math.floor(Math.random() * 1000); 
-      let tagName = "Random Tag "+random_num;
+    it('Test3: User add custom tag and check if it appears on the tag list', async () => {
+      await page.click('#add-tag');
 
       const custom_tag= await page.evaluate(() => {
-        let lists = document.getElementsByTagName("task-list").length;
-        const elem = document.querySelector("task-list:nth-child("+lists+")").shadowRoot.querySelector(".entry #tasks");
-        elem.value = "test content";
-        elem.blur();
+        let random_num = Math.floor(Math.random() * 1000); 
+        let tagName = "Random Tag "+random_num;
+        let lists = document.getElementsByTagName("custom-tag").length;
+        const elem = document.querySelector("custom-tag:nth-child("+lists+")").shadowRoot.querySelector("#custom-tags");
+        elem.value = tagName;
+
+        let tagBt = document.querySelector("custom-tag:nth-child("+lists+")").shadowRoot.querySelector("#submit");
+        tagBt.click();
         return elem.value;
       });
+
+      //change the task type
+      const tagCheck = await page.evaluate(() => {
+        let lists = document.getElementsByTagName("task-list").length;
+        const elem = document.querySelector("task-list:nth-child("+lists+")").shadowRoot.querySelector(".entry #tag-select option:last-child");
+        return elem.value;
+      });
+
+      expect(tagCheck).toBe(custom_tag);
+    });
+
+    //User add refelction on today
+    it('Test4: User add refelction on today', async () => {
+      const old_Reflect = await page.evaluate(() => {
+        let elem = document.getElementById("reflection");
+        return elem.value;
+      });
+
+      const reflection = await page.$('#reflection');
+      await reflection.type(new Date().toDateString());
+      //change the task content
+      const Reflect = await page.evaluate(() => {
+        let elem = document.getElementById("reflection");
+        return elem.value;
+      });
+      expect(Reflect).toBe(new Date().toDateString()+old_Reflect);
+
+    });
+
+    //User click on the calendar and it shows the tasks correctly
+    it('Test5: User click on the calendar and it shows the tasks correctly', async () => {
+      
 
     });
 
